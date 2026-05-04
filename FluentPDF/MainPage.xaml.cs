962dde4b06b3cefd1e889d009cf060a9467762bc
云漠星河 <yunmoxinghe@outlook.com>
2026-05-04 14:44:57 +0800

    init: initial commit

---
+++ FluentPDF/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace FluentPDF
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a <see cref="Frame">.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
